7b343ab3b53f88ef07fb019b94788c0ff73dd7a6
Dmitriy Vasilev <dmitriy.vasilev@gfk.com>
2020-11-04 12:45:08 +0100

    Added generate list

--- src/main.py
+++ src/main.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+def generate_list() -> list:
+    return list(range(1, 101))
+
 def main(args):
     print(args)
 
--- tests/test_main.py
+++ tests/test_main.py
@@ -3,11 +3,16 @@
 
 
 import unittest
-import sys
+from src.main import generate_list
 
 
 class MyTestCase(unittest.TestCase):
 
-    def test_default_greeting_set(self):
+    def test_genlist_length(self):
+        self.assertEqual(len(generate_list()), 100)
 
-        self.assertEqual(1, 2)
+    def test_genlist_first(self):
+        self.assertEqual(generate_list()[0], 1)
+
+    def test_genlist_last(self):
+        self.assertEqual(generate_list()[99], 100)
